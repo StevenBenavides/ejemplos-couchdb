f5dabe4011fc6ca691dcfb75cb5559e4ff8d6cba
Stevenbenavidesj@gmail.com <147666314+StevenBenavides@users.noreply.github.com>
2026-05-05 12:56:15 -0500

    1

--- ejemplo04/run.py
+++ ejemplo04/run.py
@@ -14,7 +14,7 @@ for d in data['docs']:
 
 base_datos = "personas004"
 # Configurar el acceso a la base de datos
-url = f"http://127.0.0.1:5984/{base_datos}"
+url = f"http://127.0.0.1:5985/{base_datos}"
 headers = {'Content-Type': 'application/json'}
 
 # Enviar datos
@@ -25,3 +25,13 @@ for doc in lista_datos:
         json=doc
     )
     print(f"Insertando {doc['nombre']} | {response.status_code}")
+
+
+
+
+"""
+Las pricipales diferencias que podemos ver es que en el ejemplo 3 se enviavan los datos 
+utilizamos para hacer la inserción de los datos utilizamos el bulk_docs enviamos todos los documentos
+fitrandolos por el HTTPS post a chouchDB  en el ejemplo 4 podemos ver que cada insercion es una por 
+una con peticiones post individuales imprimiendo cada estatusde cada insercion.
+"""
